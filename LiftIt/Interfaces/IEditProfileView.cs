437810453c24b0738fa58ae11c89534046e0037c
Skepter1 <michal.ciszewski@spoko.pl>
2026-06-20 22:37:02 +0200

    Edit profile screen improvements, dark/light mode, About & Contact screen

---
+++ LiftIt/Interfaces/IEditProfileView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiftIt.Interfaces
+{
+    internal interface IEditProfileView
+    {
+        string Name { get; set; }
+        string Email { get; set; }
+        string Password { get; set; }
+        public void SuccesfulModification(string message);
+        event Action ModificationRequested;
+    }
+}
